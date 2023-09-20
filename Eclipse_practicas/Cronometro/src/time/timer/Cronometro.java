c70e6cd17b658f0be3bd16b7f8d8962009b906f4
JosedeJesus002 <jose_1321124119@uptecamac.edu.mx>
2023-09-20 10:55:03 -0600

    Metronomo

--- Eclipse_practicas/Cronometro/src/time/timer/Cronometro.java
+++ Eclipse_practicas/Cronometro/src/time/timer/Cronometro.java
@@ -16,7 +16,6 @@ public class Cronometro {
             while (true) {
                 Thread.sleep(1000); // Espera un segundo
                 System.out.println(horas.getHoras() + ":" + minutos.getMinutos() + ":" + segundos.getSegundos());
-                new ProcessBuilder("cmd","/c","cls").inheritIO().start().waitFor();
             }
         } catch (InterruptedException e) {
             e.printStackTrace();
---
+++ Eclipse_practicas/Metronomo/src/metro/metro/MetronomoApp.java
@@ -0,0 +1,12 @@
+package metro.metro;
+
+public class MetronomoApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Metronomo metronomo = new Metronomo();
+		
+		metronomo.start();
+	}
+
+}
